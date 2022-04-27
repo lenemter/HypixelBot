348618521c8f4ba2eb7ab22859165f869c6a5f2b
lenemter <lenemter@gmail.com>
2022-04-27 19:30:49 +0900

    Fix locale for Heroku

--- hypixel-chatbot/main.py
+++ hypixel-chatbot/main.py
@@ -25,7 +25,7 @@ from bots.news import NewsBot
 from bots.settings import SettingsBot
 from bots.stats import HypixelStats
 
-locale.setlocale(locale.LC_ALL, "ru_RU")
+locale.setlocale(locale.LC_ALL, "ru_RU.utf8")
 
 
 ONLINE_MESSAGES = [
